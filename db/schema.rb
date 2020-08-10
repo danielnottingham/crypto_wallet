368fcb6798d49e5e440de89cb1356c6e250dc447
Daniel <danielnottingham.ti@gmail.com>
2020-08-10 13:51:49 -0300

    migration add mining types to coins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_142035) do
+ActiveRecord::Schema.define(version: 2020_08_10_164528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_142035) do
     t.string "url_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "mining_type_id"
+    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
   end
 
   create_table "mining_types", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_08_10_142035) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "coins", "mining_types"
 end
